efadfdeec41c440f8f189d29b99b3a20aeaf6113
Juan de Urtubey <jdeurt@gmail.com>
2022-09-15 00:05:29 -0500

    fix: fix orientation hopefully

--- Sources/FastCodeScanner/ScannerCoordinator.swift
+++ Sources/FastCodeScanner/ScannerCoordinator.swift
@@ -76,7 +76,7 @@ extension FastCodeScannerView {
             }
             
             if (parent.isScanning) {
-                let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .up, options: requestOptions)
+                let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer, orientation: .right, options: requestOptions)
                 try? imageRequestHandler.perform([self.detectBarcodeRequest])
             }
         }
